f547c0150b826fc670b3eb5120c3b42f9cfacec5
eduardofrederico <eduardo.frederico@icloud.com>
2020-12-19 22:50:48 -0300

    Criando componentes e construindo o projeto.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,3 +1,66 @@
-export class courseListComponent {
-    
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component ({
+    selector: 'app-course-list',
+    templateUrl: './course-list-component.html'
+})
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 100.00,
+                code: '2001',
+                duration: 120,
+                rating: 3.7,
+                releaseDate: 'Feb - 2020'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 120.00,
+                code: '2002',
+                duration: 128,
+                rating: 4.2,
+                releaseDate: 'Aug - 2020'
+            },
+            {
+                id: 3,
+                name: 'Salesforce',
+                imageUrl: '',
+                price: 150.00,
+                code: '2003',
+                duration: 136,
+                rating: 4.6,
+                releaseDate: 'Jan - 2021'
+            },
+            {
+                id: 4,
+                name: 'Java',
+                imageUrl: '',
+                price: 120.00,
+                code: '2004',
+                duration: 128,
+                rating: 4.1,
+                releaseDate: 'Jan - 2021'
+            },
+            {
+                id: 4,
+                name: 'Java',
+                imageUrl: '',
+                price: 120.00,
+                code: '2004',
+                duration: 128,
+                rating: 4.1,
+                releaseDate: 'Jan - 2021'
+            }
+        ]
+    }
 }

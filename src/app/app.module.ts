2325ebdcd59f75bdeb577977a193930ed0a1ae81
eduardofrederico <eduardo.frederico@icloud.com>
2020-12-21 17:56:53 -0300

    Protegendo Rotas com Guards.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,35 @@ import { FormsModule } from '@angular/forms';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { componentFactoryName } from '@angular/compiler';
+import { Erro404Component } from './Erro404/erro-404.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Erro404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

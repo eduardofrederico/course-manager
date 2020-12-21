f004d927f5764ec3bfd6be05c93193b34ebbeb23
eduardofrederico <eduardo.frederico@icloud.com>
2020-12-21 13:46:33 -0300

    Aula: O que é injeção de dependência?

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component ({
     selector: 'app-course-list',
@@ -9,58 +10,10 @@ export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) {}
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 100.00,
-                code: '2001',
-                duration: 120,
-                rating: 3.7,
-                releaseDate: 'Feb - 2020'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 120.00,
-                code: '2002',
-                duration: 128,
-                rating: 4.2,
-                releaseDate: 'Aug - 2020'
-            },
-            {
-                id: 3,
-                name: 'Salesforce',
-                imageUrl: '',
-                price: 150.00,
-                code: '2003',
-                duration: 136,
-                rating: 4.6,
-                releaseDate: 'Jan - 2021'
-            },
-            {
-                id: 4,
-                name: 'Java',
-                imageUrl: '',
-                price: 120.00,
-                code: '2004',
-                duration: 128,
-                rating: 4.1,
-                releaseDate: 'Jan - 2021'
-            },
-            {
-                id: 5,
-                name: 'Java',
-                imageUrl: '',
-                price: 120.00,
-                code: '2004',
-                duration: 128,
-                rating: 4.8,
-                releaseDate: 'Jan - 2021'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
+            
     }
 }

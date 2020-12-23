2f4ae9a32d7448f2f8ca539cbb592d87396d006f
eduardofrederico <eduardo.frederico@icloud.com>
2020-12-23 01:44:15 -0300

    Tratando erros com Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { Erro404Component } from './Erro404/erro-404.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
@@ -10,8 +9,7 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Erro404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +19,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Erro404Component
       }
     ])
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { Erro404Component } from "./component/Erro404/erro-404.component";
 import { NavBarComponent } from "./component/nav-bar/nav-bar.component";
 
 @NgModule({
     declarations: [
-        NavBarComponent
+        NavBarComponent,
+        Erro404Component
     ],
     imports: [
-        RouterModule
+        RouterModule.forChild([
+            {
+                path: '**', component: Erro404Component
+            }
+        ])
     ],
     exports: [
-        NavBarComponent
+        NavBarComponent,
     ]
 })
 export class CoreModule {
